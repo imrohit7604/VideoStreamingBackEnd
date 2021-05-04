98b02432fdb502bdeaa83e75c484989757b63a1a
rohit <imrohit7604@gmail.com>
2021-05-04 13:01:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+//End Point for getting video
+app.get('/video', (req, res) => {
+    res.sendFile('assets/finalVideo.mp4', { root: __dirname });
+});
+
+
+app.get('/downloadVideo', (req, res) => {
+    res.download(__dirname+'/assets/finalVideo.mp4',"download.mp4");
+});
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000!')
+});
